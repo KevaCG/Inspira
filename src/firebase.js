46b8b4f158be92b2d5017019de178d3b567e7532
Kevin Castrillon <kevin06092005@gmail.com>
2025-10-03 10:15:18 -0500

    Corregimos firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore"; // Importa getFirestore
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_API_KEY,
-    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_APP_ID,
-    measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+  apiKey: "AIzaSyCf7yvJe7r0Hme0guwK-1vwX0DWQesgdIE",
+  authDomain: "inspira-27ea2.firebaseapp.com",
+  projectId: "inspira-27ea2",
+  storageBucket: "inspira-27ea2.firebasestorage.app",
+  messagingSenderId: "665415657251",
+  appId: "1:665415657251:web:eb31a93a384187b1924e96",
+  measurementId: "G-XGSGT2JMTJ",
 };
 
 const app = initializeApp(firebaseConfig);
